52f7f9b6be8cf7ec5ed707f7446d13d587b10883
crc0610 <cheerc106@gmail.com>
2026-09-08 11:25:35 +1000

    Update loganalyze.sh
    added the rest

--- loganalyze.sh
+++ loganalyze.sh
@@ -21,3 +21,34 @@ fi
 files=$(find "$dir" -mindepth 1 -maxdepth 1 -name "*log" -mtime -7 ! -name "analysisData.log" ! -name "summary.log")
 > ~/analysisData.log
 > ~/summary.log
+
+if [ "$files" == "" ]; then
+    echo -e "No. of modified log files: 0\n"
+    exit 0
+else
+    total_count=0
+    largest_count=0
+    largest_log=""
+
+    for file in $files
+    do 
+        count=$(grep -i "error" "$file" | wc -l)
+        total_count=$((total_count + count))
+        
+        if [ $count -gt $largest_count ]; then
+            largest_count=$count
+            largest_log=$(echo "$file <error-count:$count>")
+        fi
+
+        echo "$file <No. of errors found = $count>"
+        echo "$file <No. of errors found = $count>" >> ~/analysisData.log
+
+    done
+
+    echo "Total errors found:$total_count"
+    echo "File with the max errors:$largest_log"
+
+    echo "Total errors found:$total_count" >> ~/summary.log
+    echo "File with the max errors:$largest_log" >> ~/summary.log
+
+fi
